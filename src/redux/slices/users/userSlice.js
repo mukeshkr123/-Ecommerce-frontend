6455eb734c0f22a4c562275e277d8cf4d0844b29
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-21 17:47:08 +0530

    add login action

--- src/redux/slices/users/userSlice.js
+++ src/redux/slices/users/userSlice.js
@@ -1,3 +1,7 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import baseUrl from "../../../utils/baseUrl";
+
 // initial state
 const initialState = {
   loading: false,
@@ -11,3 +15,19 @@ const initialState = {
     userInfo: {},
   },
 };
+
+// login action
+export const LoginUserAction = createAsyncThunk(
+  "users/login",
+  async (payload, { getState, rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${baseUrl}/users/login`, {
+        email: payload?.email,
+        password: payload?.password,
+      });
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
