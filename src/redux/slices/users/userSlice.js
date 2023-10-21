6b381177b516fed56eae7ed6f37058e9ef3c27eb
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-21 17:54:46 +0530

    add use slices

--- src/redux/slices/users/userSlice.js
+++ src/redux/slices/users/userSlice.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import baseUrl from "../../../utils/baseUrl";
 
@@ -31,3 +31,28 @@ export const LoginUserAction = createAsyncThunk(
     }
   }
 );
+
+// users slice
+
+const userSlice = createSlice({
+  name: "users",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(LoginUserAction.pending, (state, action) => {
+      state.userAuth.loading = true;
+    });
+    builder.addCase(LoginUserAction.fulfilled, (state, action) => {
+      state.userAuth.userInfo = action.payload;
+      state.userAuth.loading = false;
+    });
+    builder.addCase(LoginUserAction.rejected, (state, action) => {
+      state.userAuth.error = action.payload;
+      state.userAuth.loading = false;
+    });
+  },
+});
+
+//generate reducer
+const userReducer = userSlice.reducer;
+
+export default userReducer;
