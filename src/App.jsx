b2d831972bec64a9f681b380112c1b5b1b5dffe8
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-23 20:16:58 +0530

    Add navabar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar/Navabar";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         {/* users  */}
