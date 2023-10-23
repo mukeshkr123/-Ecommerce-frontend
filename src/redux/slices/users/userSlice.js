e071e80c8cbf061686e358842a6a88c6b7d9b653
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-23 21:12:46 +0530

    add register user slice and action

--- src/redux/slices/users/userSlice.js
+++ src/redux/slices/users/userSlice.js
@@ -34,11 +34,29 @@ export const LoginUserAction = createAsyncThunk(
   }
 );
 
+// register the user
+export const registerUserAction = createAsyncThunk(
+  "users/register",
+  async ({ fullname, email, password }, { getState, rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`${baseUrl}/users/register`, {
+        fullname,
+        email,
+        password,
+      });
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
 // users slice
 const userSlice = createSlice({
   name: "users",
   initialState,
   extraReducers: (builder) => {
+    // login
     builder.addCase(LoginUserAction.pending, (state, action) => {
       state.userAuth.loading = true;
     });
@@ -49,6 +67,18 @@ const userSlice = createSlice({
     builder.addCase(LoginUserAction.rejected, (state, action) => {
       state.userAuth.error = action.payload;
       state.userAuth.loading = false;
+    });
+    // register
+    builder.addCase(registerUserAction.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(registerUserAction.fulfilled, (state, action) => {
+      state.user = action.payload;
+      state.loading = false;
+    });
+    builder.addCase(registerUserAction.rejected, (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
     });
   },
 });
